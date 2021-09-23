4d82be39cc5f848d8a67a3dc06e2fddaccdbc499
Thakshayan <thakshayan007@gmail.com>
2021-09-23 21:35:48 +0530

    mutation

--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -2,9 +2,9 @@ import { gql } from '@apollo/client';
 
 
 const SIGNIN_USER = gql`
-  mutation signIn($email: String, $password: String!) {
-    signIn(email: $email, password: $password)
-  }
+mutation SignINSPMutation($signInspUsername: String!, $signInspPassword: String!) {
+  signINSP(username: $signInspUsername, password: $signInspPassword)
+}
 `;
 
 const SIGNUP_USER = gql`
